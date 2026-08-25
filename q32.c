3cdcb35d9b64c049091cb30f4a9780c8eb4a001e
ATHARVA25CHAUHAN <ATHARVA.40210@stu.upes.ac.in>
2026-08-25 17:10:41 +0530

    Add solutions for q29 to q32

---
+++ q32.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int n, original, rev = 0, rem;
+scanf("%d", &n);
+original = n;
+while (n != 0)
+{
+rem = n % 10;
+rev = rev * 10 + rem;
+n = n / 10;
+}
+if (original == rev)
+printf("Palindrome");
+else
+printf("Not Palindrome");
+return 0;
+}
